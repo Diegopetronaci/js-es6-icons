bf734c17261a3ab60651ad3aa17f7e5534c950ac
Diego <Petronacidiego@gmail.com>
2020-12-10 17:06:13 +0100

    commit iniziale

---
+++ es6-icons/milestone1/icons.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+  const icone = [
+    {
+      name: "gatto",
+      prefix: "fas fa-cat",
+      type: "animale",
+      family: "",
+    },
+    {
+      name: "verdura",
+      prefix: "fas fa-carrot",
+      type: "vegetale",
+      family: "",
+    },
+    {
+      name: "profilo",
+      prefix: "fas fa-user",
+      type: "profilo",
+      family: "",
+    }
+  ]
+  console.log(icone);
+  // Selezioniamo il container icons
+
+  /* const icons = $(".icons"); */
+
+  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+  /* icone.forEach(icona => {
+    let markup = `<div>
+      <i class="${icona.prefix}" style="color:blue"></i>
+      <div class="title">${icona.name}</div>
+      </div>`;
+      console.log(markup);
+      icons.append(markup);
+  }); */
+  
+  const scrivi = myfunction()
+
+});
+
+function myfunction(prefix, name) {
+  
+}
+
+/* ---- FUNCTIONS ----*/
+// Creiamo una funzione per iterare nell'array e che appenda al container le icone.
+// tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
+// tip: il template literal ci puo aiutare con il markup
+
